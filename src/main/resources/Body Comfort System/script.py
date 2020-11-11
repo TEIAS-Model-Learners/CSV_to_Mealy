8b67a408f84d955db96e556286c901b36add9680
Rasta <rasta.tadayon1378@gmail.com>
2020-11-11 11:22:42 +0330

    minor bug fixed in script

--- src/main/resources/Body Comfort System/script.py
+++ src/main/resources/Body Comfort System/script.py
@@ -10,15 +10,15 @@ import pandas as pd
 
 PATH_TO_BASE_PRODUCTS = "./Base Products/Product CSV Files"
 PATH_TO_FEATURED_PRODUCTS = "./Featured Products/Product CSV Files"
-PATH_TO_BASE_DOT_FILE = "./Base Products/Product dot Files"
-PATH_TO_FEATURED_DOT_FILES = "./Featured Products/Product dot Files"
+PATH_TO_BASE_KISS_FILES = "./Base Products/Product kiss Files"
+PATH_TO_FEATURED_KISS_FILES = "./Featured Products/Product kiss Files"
 
 
 # In[2]:
 
 
 
-def make_dot_file(data, file_name, path_to_dot_file):
+def make_kiss_file(data, file_name, path_to_kiss_file):
     file_content = 'digraph G {\nlabel=""\n'
     file_ending = "}"
     start_state = True
@@ -39,7 +39,7 @@ def make_dot_file(data, file_name, path_to_dot_file):
             file_content += state + ' -> ' + row[3] + '[label="' + row[1] + '/' + row[2] + '"]\n'
     file_content += file_ending
     
-    f = open(path_to_dot_file +'/'+ file_name +'.dot', 'w')
+    f = open(path_to_kiss_file +'/'+ file_name +'.kiss', 'w')
     f.write(file_content)
     f.close()
 
@@ -47,8 +47,14 @@ path = PATH_TO_FEATURED_PRODUCTS + "/*.csv"
 for fname in glob.glob(path):
     data = pd.read_csv(fname, header=None, index_col=None)
     print(fname.split('/')[-1])
-    make_dot_file(data, fname.split('/')[-1][:-4], PATH_TO_FEATURED_DOT_FILES)
-    
+    make_kiss_file(data, fname.split('/')[-1][:-4], PATH_TO_FEATURED_KISS_FILES)
+
+
+path = PATH_TO_BASE_PRODUCTS + "/*.csv"
+for fname in glob.glob(path):
+    data = pd.read_csv(fname, header=None, index_col=None)
+    print(fname.split('/')[-1])
+    make_kiss_file(data, fname.split('/')[-1][:-4], PATH_TO_BASE_KISS_FILES)
 
     
 
