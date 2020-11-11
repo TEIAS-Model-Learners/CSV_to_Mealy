f0971b3b8ee53680d0862ebe6bd22fba73f78bba
Rasta <rasta.tadayon1378@gmail.com>
2020-11-11 12:04:52 +0330

    kiss files corrected

--- src/main/resources/Body Comfort System/script.py
+++ src/main/resources/Body Comfort System/script.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
 
 
 import os
@@ -14,34 +11,23 @@ PATH_TO_BASE_KISS_FILES = "./Base Products/Product kiss Files"
 PATH_TO_FEATURED_KISS_FILES = "./Featured Products/Product kiss Files"
 
 
-# In[2]:
 
 
 
 def make_kiss_file(data, file_name, path_to_kiss_file):
-    file_content = 'digraph G {\nlabel=""\n'
-    file_ending = "}"
-    start_state = True
+    file_content = ""
     states = list(data[0].unique())
-    # declaring states
+
+    
     for state in states:
-        if(start_state):
-            file_content += state + ' [color="red"]\n'
-            start_state = False
-        else:
-            file_content += state + '\n'
-        
-    # creating transitions
-    for state in states:
-        file_content += state + ' [label="' + state + '"];\n'
         transitions = data[data[0] == state]
         for index, row in transitions.iterrows():
-            file_content += state + ' -> ' + row[3] + '[label="' + row[1] + '/' + row[2] + '"]\n'
-    file_content += file_ending
-    
+            file_content += row[0] + " -- " + row[1] + "/" + row[2] + " -> " + row[3] + "\n"
+        file_content += "\n\n"
     f = open(path_to_kiss_file +'/'+ file_name +'.kiss', 'w')
     f.write(file_content)
     f.close()
+    
 
 path = PATH_TO_FEATURED_PRODUCTS + "/*.csv"
 for fname in glob.glob(path):
@@ -57,10 +43,6 @@ for fname in glob.glob(path):
     make_kiss_file(data, fname.split('/')[-1][:-4], PATH_TO_BASE_KISS_FILES)
 
     
-
-
-# In[ ]:
-
 
 
 
